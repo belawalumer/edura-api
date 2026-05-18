17160f241a3e3308ed7270bfa10563097002615a
Muhammad Bilal <shiekhbilalbs5@gmail.com>
2026-05-18 17:03:03 +0500

    feat: add college and univeristy for admin

---
+++ src/colleges/dto/create-college.dto.ts
@@ -0,0 +1,26 @@
+import { IsString, IsNotEmpty, IsArray, ValidateNested, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class MeritDto {
+  @IsString()
+  @IsNotEmpty()
+  degree: string;
+
+  @IsNumber()
+  lastYearClosingMerit: number;
+}
+
+export class CreateCollegeDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => MeritDto)
+  merits: MeritDto[];
+}
---
+++ src/universities/dto/get-universities-query.dto.ts
@@ -0,0 +1,20 @@
+import { IsOptional, IsInt, Min, Max } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class GetUniversitiesQueryDto {
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Transform(({ value }) => parseInt(value, 10))
+  page?: number = 1;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @Transform(({ value }) => parseInt(value, 10))
+  limit?: number = 10;
+
+  @IsOptional()
+  search?: string;
+}
